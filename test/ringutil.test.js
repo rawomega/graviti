daee8ecff81d17499039ccb48606193d288920ce
u-phoria <uros_r@yahoo.com>
2011-02-04 09:57:58 +0000

    More ring position arithmetic stuff - nearest node

--- test/ringutil.test.js
+++ test/ringutil.test.js
@@ -1,7 +1,55 @@
-var overlayutil = require('ringutil');
+var ringutil = require('ringutil');
+var assert = require('assert');
+
+var anId = 'FA5A18416DD849ACAA55D926C2D7946064A69EF2';
+var higherId = 'F7DB7ACE15254C87B81D05DA8FA49588540B1950';
+var lowerId = '65B658373C7841A7B66521637C25069758B46189';
+var wrappedId = '0F5147A002B4482EB6D912E3E6518F5CC80EBEE6';
+var oneMoreId = 'FA5A18416DD849ACAA55D926C2D7946064A69EF3';
+var oneLessId = 'FA5A18416DD849ACAA55D926C2D7946064A69EF1';
 
 module.exports = {
-	shouldSelectSelfAsNearestNodeIfNoOtherNodes : function() {
+	shouldFindNoNearestIdWhenIdSetUndefined: function() {
+		assert.eql(undefined, ringutil.getNearestId(anId, undefined));
+	},
 	
+	shouldFindNoNearestIdWhenIdSetEmpty : function() {
+		assert.eql(undefined, ringutil.getNearestId(anId, []));
+	},
+	
+	shouldFindNoNearestIdWhenIdSetContainsSelf : function() {
+		assert.eql(anId, ringutil.getNearestId(anId, [anId]));
+	},
+	
+	shouldFindNoNearestIdWhenIdSetContainsHigherId  : function() {
+		assert.eql(higherId, ringutil.getNearestId(anId, [higherId]));
+	},
+	
+	shouldFindNoNearestIdWhenIdSetContainsLowerId  : function() {
+		assert.eql(lowerId, ringutil.getNearestId(anId, [lowerId]));
+	},
+	
+	shouldFindNoNearestIdWhenItIsWrappedCW : function() {
+		assert.eql(wrappedId, ringutil.getNearestId(anId, [lowerId, wrappedId]));
+	},
+	
+	shouldFindNoNearestIdFromThree : function() {
+		assert.eql(higherId, ringutil.getNearestId(anId, [lowerId, higherId, wrappedId]));
+	},
+	
+	shouldGiveNearestIdClockwiseWhenSameDistance : function() {
+		var lowerFirst = ringutil.getNearestId(anId, [oneLessId, oneMoreId]);
+		var higherFirst = ringutil.getNearestId(anId, [oneMoreId, oneLessId]);
+		
+		assert.eql(oneMoreId, lowerFirst);
+		assert.eql(oneMoreId, higherFirst);
+	},
+	
+	shouldBeAbleToGetNearestIdOrSelfWhenNoIdsGiven : function() {
+		assert.eql(anId, ringutil.getNearestIdOrSelf(anId, undefined));
+	},
+	
+	shouldBeAbleToGetNearestIdOrSelfWhenIdsGiven : function() {
+		assert.eql(higherId, ringutil.getNearestIdOrSelf(anId, [higherId, lowerId]));
 	}
 };
