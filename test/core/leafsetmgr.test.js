17c3b051eeadd6745ac158cd189d1cc15277710d
u-phoria <uros_r@yahoo.com>
2011-02-19 22:30:43 +0000

    Do not add myself to leafset

--- test/core/leafsetmgr.test.js
+++ test/core/leafsetmgr.test.js
@@ -3,6 +3,7 @@ var leafsetmgr = require('core/leafsetmgr');
 var node = require('core/node');
 var testCase = require('nodeunit').testCase;
 
+var myId = '3909DB380AF909E320329511CC932099BAD10094';
 var anId = 'F45A18416DD849ACAA55D926C2D7946064A69EF2';
 var higherId = 'F7DB7ACE15254C87B81D05DA8FA49588540B1950';
 var lowerId = '65B658373C7841A7B66521637C25069758B46189';
@@ -10,11 +11,10 @@ var wrappedId = '0F5147A002B4482EB6D912E3E6518F5CC80EBEE6';
 var oneMoreId = 'F45A18416DD849ACAA55D926C2D7946064A69EF3';
 var oneLessId = 'F45A18416DD849ACAA55D926C2D7946064A69EF1';
 
-node.nodeId = anId;
-
 module.exports = {
 	"calculating next routing hop" : testCase ({
 		setUp : function(done) {
+			node.nodeId = anId;
 			leafsetmgr.leafset = {};		
 			done();
 		},
@@ -99,6 +99,7 @@ module.exports = {
 	
 	"updating the leafset" : testCase ({
 		setUp : function(done) {
+			node.nodeId = myId;
 			leafsetmgr.leafset = {};		
 			done();
 		},
@@ -139,6 +140,18 @@ module.exports = {
 			test.equal(2, Object.keys(leafsetmgr.leafset).length);
 			test.equal('1.2.3.4', leafsetmgr.leafset[anId]);
 			test.equal('2.4.6.8', leafsetmgr.leafset[higherId]);
+			test.done();
+		},
+		
+		"should disregaard own node id when updating existing leafset" : function(test) {
+			leafsetmgr.leafset[lowerId] = "1.2.3.4";
+			leafsetmgr.leafset[anId] = "2.3.4.5";
+			
+			leafsetmgr.updateLeafset(myId, '3.4.5.6');
+			
+			test.equal(2, Object.keys(leafsetmgr.leafset).length);
+			test.equal('1.2.3.4', leafsetmgr.leafset[lowerId]);
+			test.equal('2.3.4.5', leafsetmgr.leafset[anId]);
 			test.done();
 		},
 		
