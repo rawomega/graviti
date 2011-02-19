356deb8006bf037f0a1d5911033f04a855fd5213
u-phoria <uros_r@yahoo.com>
2011-02-19 22:30:57 +0000

    First integration test for leafsets

--- test/integration/ring.management.test.js
+++ test/integration/ring.management.test.js
@@ -1,23 +1,43 @@
 var util = require('util');
 var multinode = require('testability/multinode');
+var nodeunit = require('nodeunit');
 
-module.exports = {	
-	"start and stop a multi-node ring" : multinode.testCase({
-		node_ids : [
-		    '0000000000000000000000000000000000000000',
-		    '4444444444444444444444444444444444444444',
-		    '8888888888888888888888888888888888888888',
-		    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
-		],
+module.exports = {
+	"start and stop a multi-node ring" : nodeunit.testCase({
+		setUp : function(done) {
+			this.nodeIds = [
+			    '0000000000000000000000000000000000000000',
+			    '4444444444444444444444444444444444444444',
+			    '8888888888888888888888888888888888888888',
+			    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
+			];
+			this.nodes = multinode.start({
+				node_ids : this.nodeIds
+			});
+			done();
+		},
+		
+		tearDown : function(done) {
+			multinode.stop(this.nodes);
+			done();
+		},
 
 		"populate leafsets" : function(test) {
-			var leafsetCheck = function() {
+			var _this = this;
+			var getLeafsetSize = function() {
 				return Object.keys(require('core/leafsetmgr').leafset).length;
 			};
-
-			multinode.waitUntilEqual( 3, leafsetCheck, 0, function(res, msg) {
-				test.ok(res, msg);
-				test.done();
+			var getLeafset = function() {
+				return require('core/leafsetmgr').leafset;
+			};
+			this.nodes[0].waitUntilEqual(3, getLeafsetSize, test, function() {
+				_this.nodes[3].eval(getLeafset, test, function(res) {
+					test.equal(3, Object.keys(res).length);
+					test.ok(res[_this.nodeIds[0]] !== undefined);
+					test.ok(res[_this.nodeIds[1]] !== undefined);
+					test.ok(res[_this.nodeIds[2]] !== undefined);
+					test.done();
+				});
 			});
 		},
 
