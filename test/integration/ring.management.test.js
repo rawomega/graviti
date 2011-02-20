fa0445a2db5c8d4875a7c015f9622283dc0830a6
u-phoria <uros_r@yahoo.com>
2011-02-20 23:55:27 +0000

    Slightly optimised evalsvr and evalclient to more eagerly close connections

--- test/integration/ring.management.test.js
+++ test/integration/ring.management.test.js
@@ -22,7 +22,7 @@ module.exports = {
 			done();
 		},
 
-		"populate leafsets" : function(test) {
+		"should populate leafsets after bootstrapping" : function(test) {
 			var _this = this;
 			var getLeafsetSize = function() {
 				return Object.keys(require('core/leafsetmgr').leafset).length;
@@ -30,19 +30,28 @@ module.exports = {
 			var getLeafset = function() {
 				return require('core/leafsetmgr').leafset;
 			};
+			var getRoutingTable = function() {
+				return require('core/routingmgr').routingTable;
+			};
+			
 			this.nodes[0].waitUntilEqual(3, getLeafsetSize, test, function() {
+				
+				// leafset populated
 				_this.nodes[3].eval(getLeafset, test, function(res) {
 					test.equal(3, Object.keys(res).length);
 					test.ok(res[_this.nodeIds[0]] !== undefined);
 					test.ok(res[_this.nodeIds[1]] !== undefined);
 					test.ok(res[_this.nodeIds[2]] !== undefined);
-					test.done();
+					
+					// routing table populated
+					_this.nodes[3].eval(getRoutingTable, test, function(res) {
+						test.equal(_this.nodeIds[0], res[0][0].id);
+						test.equal(_this.nodeIds[1], res[0][4].id);
+						test.equal(_this.nodeIds[2], res[0][8].id);
+						test.done();
+					});
 				});
 			});
-		},
-
-		"something else" : function(test) {
-			test.done();
 		}
 	})
 };
